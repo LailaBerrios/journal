d262364c32a789c2fb3c45a9911f44b769863514
Joyce Mayorga <joycem137@gmail.com>
2016-03-20 23:42:49 -0700

    Added in web pack to support es6.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var fs = require('fs-extra');
+var webpack = require('webpack');
+var path = require('path');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+    .filter(function(x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function(mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
+
+const OUTPUT_DIR = path.join(__dirname, 'build');
+
+module.exports = {
+    entry: './server/server.js',
+    target: 'node',
+    output: {
+        path: OUTPUT_DIR,
+        filename: 'server.js'
+    },
+    externals: nodeModules,
+
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, loader: "style-loader!css-loader" }
+        ]
+    },
+
+    plugins: [
+        new webpack.IgnorePlugin(/\.(css|less)$/),
+        new webpack.BannerPlugin('require("source-map-support").install();',
+            { raw: true, entryOnly: false })
+    ],
+    devtool: 'sourcemap',
+
+    devServer: {
+        contentBase: OUTPUT_DIR
+    }
+};
