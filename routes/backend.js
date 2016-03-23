15feb3b986f75d9a8ed12c846081607576c708b7
Joyce Mayorga <joycem137@gmail.com>
2016-03-23 02:09:18 -0700

    It's not a route, so moved it out of route code.

--- routes/backend.js
+++ routes/backend.js
@@ -2,7 +2,7 @@
  * Our server backend.
  */
 //Lets require/import the HTTP module
-const database = require('./database');
+const database = require('./util/database');
 
 const express = require('express');
 const router = express.Router();
