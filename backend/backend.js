d8214c025218bd3f371593f7eec0bb7d9557e48c
Joyce Mayorga <joycem137@gmail.com>
2016-03-23 00:46:09 -0700

    Did simple switchover.

--- backend/backend.js
+++ backend/backend.js
@@ -2,9 +2,9 @@
  * Our server backend.
  */
 //Lets require/import the HTTP module
-const http = require('http');
-const dispatcher = require('httpdispatcher');
 const database = require('./database');
+const express = require('express');
+const app = express();
 
 //Lets define a port we want to listen to
 const PORT=process.env.PORT || 5000;
@@ -24,11 +24,10 @@ function handleNotFoundError(err, response) {
 }
 
 //Return requests for the journal data.
-dispatcher.onGet("/", function(request, response) {
+app.get("/", function(request, response) {
     database.open()
         .then((database) => {
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(JSON.stringify(database));
+            response.json(database);
             response.end();
         })
         .catch((error) => {
@@ -37,12 +36,11 @@ dispatcher.onGet("/", function(request, response) {
 });
 
 //Return requests for the journal d`ata.
-dispatcher.onGet("/entry", function(request, response) {
+app.get("/entry", function(request, response) {
     const entryIdString = request.params.entry;
     database.getEntry(entryIdString)
         .then((entry) => {
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(JSON.stringify(entry));
+            response.json(entry);
             response.end();
         })
         .catch((error) => {
@@ -50,14 +48,13 @@ dispatcher.onGet("/entry", function(request, response) {
         });
 });
 
-dispatcher.onPost('/entry', function(request, response) {
+app.post('/entry', function(request, response) {
     const entryIdString = request.params.entry;
     database.addEntry(entryIdString, request.params)
         .then((entry) => {
             return database.close()
                 .then(() => {
-                    response.writeHead(200, {'Content-Type': 'application/json'});
-                    response.write(JSON.stringify(entry));
+                    response.json(entry);
                     response.end();
                 });
         })
@@ -66,27 +63,8 @@ dispatcher.onPost('/entry', function(request, response) {
         });
 });
 
-dispatcher.onError(function(request, response) {
-    handleNotFoundError('Path not found', response);
-});
-
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    try {
-        //log the request on console
-        console.log(request.url);
-        //Disptach
-        dispatcher.dispatch(request, response);
-    } catch(err) {
-        console.error(err);
-    }
-}
-
-//Create a server
-const server = http.createServer(handleRequest);
-
 //Lets start our server
-server.listen(PORT, function(){
+app.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
